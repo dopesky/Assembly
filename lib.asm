5199962150403aa6485c9bb5dd6f749d21fe6fd4
Dopesky <oboke69@gmail.com>
2019-10-29 17:01:33 +0300

    First Commit

---
+++ lib.asm
@@ -0,0 +1,41 @@
+%macro define_byte 3 ; defines a byte and its length.
+  %1 db %2
+  %3 equ $- %1 
+%endmacro
+%macro define_word_array 2 ; defines a zero-filled array of length %2
+  %1 times %2 dw 0
+%endmacro
+%macro cout 2 ; outputs %1 to console.
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, %1
+  mov edx, %2
+  int 80h
+%endmacro
+%macro cin 2 ; prompts user for input and stores it at the top of the stack
+  mov eax, 3
+  mov ebx, 0
+  mov ecx, %1
+  mov edx, %2
+  int 80h
+  lea esi, [%1]
+  call readString
+  push ebx
+%endmacro
+%macro exit 1 ; exits the program with exit code %1
+  mov eax, 1
+  mov ebx, %1
+  int 80h
+%endmacro
+readString:
+  xor ebx,ebx
+  .next_digit:
+    movzx eax, byte [esi]
+    inc esi
+    sub al, '0'
+    imul ebx, 10
+    add ebx, eax
+    cmp byte [esi], 0xA
+    jne .next_digit
+    ret
+
---
+++ math.asm
@@ -0,0 +1,54 @@
+%macro add_nums 2 ; adds 2 numbers from the top of the stack and stores the result in %1
+  pop eax
+  pop ebx
+  add eax, ebx
+  mov edi, %1
+  call num2str
+  sub edi, %1
+  mov [%2], edi
+  int 80h
+%endmacro
+
+%macro multiply_nums 2 ; multiplies 2 numbers from the top of the stack and stores the result in %1
+  pop eax
+  pop ebx
+  mul ebx
+  mov edi, %1
+  call num2str
+  sub edi, %1
+  mov [%2], edi
+  int 80h
+%endmacro
+%macro divide_nums 2 ;divides 2 numbers from the top of the stack and stores the result in %1
+  mov edx, 0
+  pop ebx
+  pop eax
+  div ebx
+  mov edi, %1
+  call num2str
+  sub edi, %1
+  mov [%2], edi
+  int 80h
+%endmacro
+
+num2str: ; converts the byte in eax register to a string in the edi register
+  xor ecx, ecx ; clear the ecx register
+  mov ebx, 10 ; initialize the divisor to 10
+  .loop1:
+    xor edx, edx
+    div ebx ; divide the contents of eax with ebx.
+    push edx ; push the remainder to the stack
+    inc ecx ; increment the counter so we use it in loop2
+    test eax, eax ; test if eax is 0 to indicate end of loop
+    jnz .loop1
+
+  .loop2:
+    pop edx ; pop the top of stack
+    add edx, '0' ; we assume the result was in decimal thus we convert it back to ascii;
+    mov [edi], edx
+    inc edi
+    loop .loop2
+  mov byte [edi], 0xa
+  inc edi
+  mov byte [edi], 0
+  ret
